be7c23644edb6b057612afedf8b47615ade816aa
Andrexxy <andre.alves.silva27@escola.pr.gov.br>
2025-10-01 09:06:40 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,31 @@
+criaCartao(
+    categoria,
+    pergunta,
+    resposta,
+)
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação.',
+)
+criaCartao(
+    'Programação',
+    'O que é CSS?',
+    'O CSS é uma linguagem de estilização.',
+)
+criaCartao(
+    'Matemática',
+    'Qual o conceito da matemática?',
+    'Análise e identificação de padrões.',
+)
+criaCartao(
+    'Matemática',
+    'Qual a raiz quadrada de 81?',
+    '9.',
+)
+criaCartao(
+    'Matemática',
+    'Qual é o valor aproximado de pi?',
+    '3,14.',
+)
+
